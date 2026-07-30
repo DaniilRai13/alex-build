6daba564722be73ba01b4c91f5068504797284b3
DaniilRai13 <testnet02@mail.ru>
2026-07-30 21:10:29 +0300

    feat: add brand logo and KS favicons

--- src/components/ui/Logo/Logo.tsx
+++ src/components/ui/Logo/Logo.tsx
@@ -1,12 +1,12 @@
 import { type FC } from 'react';
 import { Link } from 'react-router-dom';
-import styles from './Logo.module.scss'
-import { Icon } from '@/components/ui/LucidIcon/Icon';
+import styles from './Logo.module.scss';
+import logo from '@/assets/logo.png';
+
 const Logo: FC = () => {
 	return (
-		<Link to='/' className={styles.logo}>
-			<Icon icon='HouseHeart' />
-			<span className={styles.logoText}>LOGO</span>
+		<Link to='/' className={styles.logo} aria-label='KartStav – domů'>
+			<img src={logo} alt='KartStav' className={styles.logoImg} />
 		</Link>
 	);
 };
